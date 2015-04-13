71d9876f66ee0e04d4657999bacf0d54d0f6f12e
Michele Randi <michele.randi@gmail.com>
2015-04-13 17:42:32 +0200

    use always default credential provider chain https://github.com/zalando-stups/fullstop/issues/26

--- fullstop-processing/src/main/java/org/zalando/stups/fullstop/ExtPropertiesFileConfiguration.java
+++ fullstop-processing/src/main/java/org/zalando/stups/fullstop/ExtPropertiesFileConfiguration.java
@@ -131,14 +131,7 @@ public class ExtPropertiesFileConfiguration implements ProcessingConfiguration {
         this.sqsUrl = prop.getProperty(SQS_URL);
         LibraryUtils.checkArgumentNotNull(this.sqsUrl, "Cannot find SQS URL in properties file.");
 
-        String accessKey = prop.getProperty(ACCESS_KEY);
-        String secretKey = prop.getProperty(SECRET_KEY);
-
-        if (accessKey != null && secretKey != null) {
-            this.awsCredentialsProvider = new SimplePropertiesCredentials(prop);
-        } else {
-            this.awsCredentialsProvider = credentialProvider;
-        }
+        this.awsCredentialsProvider = credentialProvider;
 
         this.s3Region = prop.getProperty(S3_REGION);
         this.visibilityTimeout = this.getIntProperty(prop, VISIBILITY_TIMEOUT);
