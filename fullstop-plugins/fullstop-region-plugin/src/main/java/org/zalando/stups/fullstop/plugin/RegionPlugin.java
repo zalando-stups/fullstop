64cfb6260e599f7b73f6361681bcc6ff89015e09
Gregsen <gkneitschel@gmail.com>
2015-04-15 16:33:19 +0200

    #13 added plugin for whitelisted regions.
    Regions can be added in the application.yaml.

---
+++ fullstop-plugins/fullstop-region-plugin/src/main/java/org/zalando/stups/fullstop/plugin/RegionPlugin.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2015 Zalando SE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.zalando.stups.fullstop.plugin;
+
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEventData;
+
+import com.jayway.jsonpath.JsonPath;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+/**
+ * @author gkneitschel
+ */
+@Component public class RegionPlugin implements FullstopPlugin {
+
+    private static final Logger LOG = LoggerFactory.getLogger(
+            RegionPlugin.class);
+
+    private static final String EC2_SOURCE_EVENTS = "ec2.amazonaws.com";
+    private static final String EVENT_NAME = "RunInstances";
+
+
+    @Value("${fullstop.pluign.properties.whitelistedRegions}")
+    private List<String> whitelistedRegions;
+
+
+    @Override public boolean supports(final CloudTrailEvent event) {
+        CloudTrailEventData cloudTrailEventData = event.getEventData();
+        String eventSource = cloudTrailEventData.getEventSource();
+        String eventName = cloudTrailEventData.getEventName();
+
+        return eventSource.equals(EC2_SOURCE_EVENTS) &&
+            eventName.equals(EVENT_NAME);
+    }
+
+    @Override public Object processEvent(final CloudTrailEvent event) {
+
+        String parameters = event.getEventData().getResponseElements();
+
+        String region = getRegion(parameters);
+        String instances = getInstanceIds(parameters).toString();
+
+        if (!whitelistedRegions.contains(region)) {
+            LOG.error("EC2 instances " + instances +
+                " are running in the wrong region! (" + region + ")");
+
+            return "EC2 instances " + instances +
+                " are running in the wrong region! (" + region + ")";
+        }
+
+        return "All clear :)";
+    }
+
+    private String getRegion(final String parameters) {
+
+        if (parameters == null) {
+            return null;
+        }
+
+        return JsonPath.read(parameters, "$.awsRegion");
+    }
+
+    private List<String> getInstanceIds(final String parameters) {
+
+        if (parameters == null) {
+            return null;
+        }
+
+        return JsonPath.read(parameters, "$.instancesSet.items[*].instanceId");
+    }
+}
