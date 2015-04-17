8849e5da152d86e8957e16c8d6e46b8061d6d1d7
Gregsen <gkneitschel@gmail.com>
2015-04-17 17:31:02 +0200

    #14 vpcs are printed to stdout

--- fullstop-plugins/fullstop-publicVPC-plugin/src/main/java/org/zalando/stups/fullstop/plugin/PublicVpc.java
+++ fullstop-plugins/fullstop-publicVPC-plugin/src/main/java/org/zalando/stups/fullstop/plugin/PublicVpc.java
@@ -16,34 +16,26 @@
 
 package org.zalando.stups.fullstop.plugin;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
-import org.springframework.stereotype.Component;
-
-import org.zalando.stups.fullstop.aws.ClientProvider;
-
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEventData;
 import com.amazonaws.services.ec2.AmazonEC2Client;
-import com.amazonaws.services.ec2.model.DescribeImagesRequest;
-import com.amazonaws.services.ec2.model.DescribeImagesResult;
-import com.amazonaws.services.ec2.model.Image;
-
-import com.google.common.collect.Lists;
-
+import com.amazonaws.services.ec2.model.DescribeVpcsResult;
+import com.amazonaws.services.ec2.model.Vpc;
 import com.jayway.jsonpath.JsonPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.zalando.stups.fullstop.aws.ClientProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @author  mrandi
+ * @author mrandi
  */
 @Component
 public class PublicVpc extends AbstractFullstopPlugin {
@@ -77,61 +69,19 @@ public class PublicVpc extends AbstractFullstopPlugin {
 
         String parameters = event.getEventData().getResponseElements();
 
-        List<String> amis = getAmi(parameters);
+        List<String> instanceIds = getInstanceId(parameters);
+        List<Vpc> vpcs = new ArrayList<Vpc>();
 
-        final List<String> whitelistedAmis = Lists.newArrayList();
-
-        AmazonEC2Client ec2Client = cachingClientProvider.getClient(AmazonEC2Client.class, whitelistedAmiAccount,
-                Region.getRegion(Regions.fromName(event.getEventData().getAwsRegion())));
-
-        DescribeImagesRequest describeImagesRequest = new DescribeImagesRequest().withOwners(whitelistedAmiAccount);
-
-        DescribeImagesResult describeImagesResult = ec2Client.describeImages(describeImagesRequest);
-        List<Image> images = describeImagesResult.getImages();
-
-        for (Image image : images) {
-            if (image.getName().startsWith("Taupage-")) {
-                whitelistedAmis.add(image.getImageId());
-            }
+        for (String instanceId : instanceIds) {
+            AmazonEC2Client amazonEC2Client = cachingClientProvider.getClient(AmazonEC2Client.class, event.getEventData().getAccountId(), Region.getRegion(Regions.fromName(event.getEventData().getAwsRegion())));
+            DescribeVpcsResult describeVpcsResult = amazonEC2Client.describeVpcs();
+            vpcs = describeVpcsResult.getVpcs();
         }
+        LOG.info("VPCs: " + vpcs.toString());
 
-        List<String> invalidAmis = Lists.newArrayList();
-
-        for (String ami : amis) {
-
-            boolean valid = false;
-
-            for (String whitelistedAmi : whitelistedAmis) {
-                if (ami.equals(whitelistedAmi)) {
-                    valid = true;
-                }
-            }
-
-            if (!valid) {
-                invalidAmis.add(ami);
-            }
-
-        }
-
-// if (!CollectionUtils.isEmpty(invalidAmis)) {
-// LOG.info("Instances with ids: {} was started with wrong images: {}", getInstanceId(parameters),
-// invalidAmis);
-// return "Instances with ids: " + getInstanceId(parameters) + " was started with wrong images: "
-// + invalidAmis;
-// } else {
-// LOG.info("Ami for instance: {} is whitelisted.", getInstanceId(parameters));
-// return "Ami for instance: " + getInstanceId(parameters) + " is whitelisted.";
-// }
 
     }
 
-    private List<String> getAmi(final String parameters) {
-        if (parameters == null) {
-            return null;
-        }
-
-        return JsonPath.read(parameters, "$.instancesSet.items[*].imageId");
-    }
 
     private List<String> getInstanceId(final String parameters) {
         if (parameters == null) {
