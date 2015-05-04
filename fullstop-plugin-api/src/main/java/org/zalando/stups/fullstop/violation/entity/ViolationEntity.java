ff39d3e0c86db838e42065d016d50f4107b5d401
Gregsen <gkneitschel@gmail.com>
2015-05-04 14:47:00 +0200

    #76 setup for using db backend to store violations

---
+++ fullstop-plugin-api/src/main/java/org/zalando/stups/fullstop/violation/entity/ViolationEntity.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2015 Zalando SE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.zalando.stups.fullstop.violation.entity;
+
+import com.google.common.base.MoreObjects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * Created by gkneitschel.
+ */
+@Entity
+public class ViolationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String accountId;
+    private String region;
+    private String message;
+    private Object violationObject;
+
+    protected ViolationEntity(){}
+
+    public ViolationEntity(final String accountId, final String region, final String message, final Object violationObject){
+
+        this.accountId = accountId;
+        this.region = region;
+        this.message = message;
+        this.violationObject = violationObject;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).omitNullValues().add("accountId", accountId).add("region", region)
+                .add("message", message).add("violationObject", violationObject).toString();
+    }
+}
