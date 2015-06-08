26a664fa7e860902e317484de79162162e56d012
jbellmann <joerg.bellmann@zalando.de>
2015-06-08 17:48:42 +0200

    fix security-auditor-aware, missing deps in pom

--- fullstop/src/main/java/org/zalando/stups/fullstop/jpa/auditing/SpringSecurityAuditorAware.java
+++ fullstop/src/main/java/org/zalando/stups/fullstop/jpa/auditing/SpringSecurityAuditorAware.java
@@ -16,13 +16,16 @@
 
 package org.zalando.stups.fullstop.jpa.auditing;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import org.slf4j.Logger;
+
 import org.springframework.data.domain.AuditorAware;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.util.Assert;
 
-import static org.slf4j.LoggerFactory.getLogger;
+import org.springframework.util.Assert;
 
 /**
  * @author  <a href="mailto:michele.randi@zalando.de" title="Michele Randi">mrandi</a>
@@ -34,15 +37,18 @@ public final class SpringSecurityAuditorAware implements AuditorAware<String> {
     @Override
     public String getCurrentAuditor() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return "FULLSTOP";
+        } else {
+            Assert.notNull(authentication, "Current authentication is null; could not gather user details from it");
 
-        Assert.notNull(authentication, "Current authentication is null; could not gather user details from it");
+            final String userName = authentication.getName();
+            logger.trace("Found Auditor: {}", userName);
 
-        final String userName = authentication.getName();
-        logger.trace("Found Auditor: {}", userName);
+            Assert.hasText(userName, "Username should never by empty");
 
-        Assert.hasText(userName, "Username should never by empty");
-
-        return userName;
+            return userName;
+        }
     }
 
 }
