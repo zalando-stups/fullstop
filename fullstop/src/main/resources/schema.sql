4ec1dd4a697fca3252c71cfd7ca866864b2ddd21
Gregsen <gkneitschel@gmail.com>
2015-06-09 10:13:56 +0200

    #82 - new tables in db for lifecycle

--- fullstop/src/main/resources/schema.sql
+++ fullstop/src/main/resources/schema.sql
@@ -18,3 +18,31 @@ CREATE TABLE IF NOT EXISTS fullstop_data.violation (
   -- TODO: we should think about that
   CONSTRAINT unique_violation UNIQUE (account_id, region, event_id, message)
 );
+
+CREATE TABLE IF NOT EXISTS fullstop_data.application (
+  id       SERIAL NOT NULL PRIMARY KEY,
+  app_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS fullstop_data.app_version (
+  id      SERIAL NOT NULL PRIMARY KEY,
+  version TEXT
+);
+
+CREATE TABLE IF NOT EXISTS fullstop_data.app_has_version(
+  id SERIAL NOT NULL PRIMARY KEY,
+  app_id INTEGER,
+  version_id INTEGER,
+  FOREIGN KEY (app_id) REFERENCES fullstop_data.application(id),
+  FOREIGN KEY (version_id) REFERENCES fullstop_data.app_version(id)
+);
+
+CREATE TABLE IF NOT EXISTS fullstop_data.lifecycle (
+  id         SERIAL NOT NULL PRIMARY KEY,
+  startdate  DATE,
+  enddate    DATE,
+  region     TEXT,
+  app_version_id     INTEGER,
+  event_type TEXT,
+  FOREIGN KEY (app_version_id) REFERENCES fullstop_data.app_has_version(id)
+);
