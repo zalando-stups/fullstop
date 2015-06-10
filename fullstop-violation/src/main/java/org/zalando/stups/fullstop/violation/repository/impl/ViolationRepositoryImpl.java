efc4655741d97b60b69f8b2bb680d0124fd371e1
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-06-10 16:25:27 +0200

    fixed pageable

--- fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/repository/impl/ViolationRepositoryImpl.java
+++ fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/repository/impl/ViolationRepositoryImpl.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,27 +15,36 @@
  */
 package org.zalando.stups.fullstop.violation.repository.impl;
 
+import static com.google.common.collect.Iterables.isEmpty;
+import static com.google.common.collect.Lists.newArrayList;
+import static com.mysema.query.types.ExpressionUtils.allOf;
+import static java.util.Collections.emptyList;
+
+import java.util.Date;
+import java.util.List;
+
 import com.mysema.query.jpa.JPQLQuery;
 import com.mysema.query.types.Predicate;
 import org.joda.time.DateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport;
 import org.zalando.stups.fullstop.violation.entity.QViolationEntity;
 import org.zalando.stups.fullstop.violation.entity.ViolationEntity;
 import org.zalando.stups.fullstop.violation.repository.ViolationRepositoryCustom;
 
-import java.util.Date;
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static com.mysema.query.types.ExpressionUtils.allOf;
-
 /**
  * Created by mrandi.
  */
+@SuppressWarnings("unused")
 public class ViolationRepositoryImpl extends QueryDslRepositorySupport implements ViolationRepositoryCustom {
+
+    private static final String ID = "id";
+
+    private static final Sort SORT_BY_ID = new Sort(ID);
 
     public ViolationRepositoryImpl() {
         super(ViolationEntity.class);
@@ -48,32 +57,40 @@ public class ViolationRepositoryImpl extends QueryDslRepositorySupport implement
 
         final JPQLQuery query = from(qViolationEntity);
 
-        final List<Predicate> mainPredicates = newArrayList();
+        final List<Predicate> predicates = newArrayList();
 
         if (accounts != null) {
-            mainPredicates.add(qViolationEntity.accountId.in(accounts));
+            predicates.add(qViolationEntity.accountId.in(accounts));
         }
 
         if (since != null) {
-            mainPredicates.add(qViolationEntity.created.after(new DateTime(since)));
+            predicates.add(qViolationEntity.created.after(new DateTime(since)));
         }
 
         if (lastViolation != null) {
-            mainPredicates.add(qViolationEntity.id.goe(lastViolation));
+            predicates.add(qViolationEntity.id.goe(lastViolation));
         }
 
-        if (checked != null && checked) {
-            mainPredicates.add(qViolationEntity.comment.isNotEmpty());
-        }
-        else {
-            // both right?
+        if (checked != null) {
+            if (checked) {
+                predicates.add(qViolationEntity.comment.isNotEmpty());
+            }
+            else {
+                predicates.add(qViolationEntity.comment.isNull().or(qViolationEntity.comment.isEmpty()));
+            }
         }
 
-        List<ViolationEntity> list = query.where(allOf(mainPredicates)).orderBy(qViolationEntity.id.asc(),
-                qViolationEntity.created.asc())
-                .list(qViolationEntity);
+        final long total = query.where(allOf(predicates)).count();
 
-        // TODO: fix pageable
-        return new PageImpl<>(list);
+        final Sort sort = pageable.getSort();
+        final Sort fixedSort = (sort == null || isEmpty(sort)) ? SORT_BY_ID : sort;
+        final PageRequest fixedPage = new PageRequest(pageable.getPageNumber(), pageable.getPageSize(), fixedSort);
+
+        getQuerydsl().applyPagination(fixedPage, query);
+
+        final List<ViolationEntity> list;
+        list = total > 0 ? query.where(allOf(predicates)).list(qViolationEntity) : emptyList();
+
+        return new PageImpl<>(list, fixedPage, total);
     }
 }
