58079a74a05a193bf4de08046c2e54f97b1ed8f8
Gregsen <gkneitschel@gmail.com>
2015-06-18 14:09:53 +0200

    #82 - added many-to-many relation between application and version. Added respositories for app, version and lifecycle. Fixed license header

--- fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/entity/ApplicationEntity.java
+++ fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/entity/ApplicationEntity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.zalando.stups.fullstop.violation.entity;
 
 import com.google.common.base.MoreObjects;
@@ -5,9 +20,11 @@ import org.zalando.stups.fullstop.violation.domain.AbstractModifiableEntity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Collection;
 
 /**
  * Created by gkneitschel.
@@ -20,7 +37,11 @@ public class ApplicationEntity extends AbstractModifiableEntity {
     private String appName;
 
     @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "app_has_version")
+    @JoinTable(name = "app_has_version",
+            joinColumns = @JoinColumn(name ="app_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "app_version_id", referencedColumnName = "id"))
+    private Collection<ApplicationVersionEntity> appVersions;
+
 
     public ApplicationEntity(String appName) {
         this.appName = appName;
@@ -32,6 +53,15 @@ public class ApplicationEntity extends AbstractModifiableEntity {
 
     public void setAppName(String appName) {
         this.appName = appName;
+    }
+
+    public Collection<ApplicationVersionEntity> getAppVersions() {
+        return appVersions;
+    }
+
+    public void setAppVersions(
+            Collection<ApplicationVersionEntity> appVersions) {
+        this.appVersions = appVersions;
     }
 
     @Override
--- fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/entity/ApplicationVersionEntity.java
+++ fullstop-violation/src/main/java/org/zalando/stups/fullstop/violation/entity/ApplicationVersionEntity.java
@@ -1,10 +1,30 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.zalando.stups.fullstop.violation.entity;
 
 import com.google.common.base.MoreObjects;
 import org.zalando.stups.fullstop.violation.domain.AbstractModifiableEntity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.Collection;
 
 /**
  * Created by gkneitschel.
@@ -13,6 +33,12 @@ import javax.persistence.Table;
 @Entity
 public class ApplicationVersionEntity extends AbstractModifiableEntity {
     private String applicationVersion;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "app_has_version",
+            joinColumns = @JoinColumn(name = "app_version_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "app_id", referencedColumnName = "id"))
+    private Collection<ApplicationVersionEntity> application;
 
     public ApplicationVersionEntity(String applicationVersion) {
         this.applicationVersion = applicationVersion;
@@ -24,6 +50,15 @@ public class ApplicationVersionEntity extends AbstractModifiableEntity {
 
     public String getApplicationVersion() {
         return applicationVersion;
+    }
+
+    public Collection<ApplicationVersionEntity> getApplication() {
+        return application;
+    }
+
+    public void setApplication(
+            Collection<ApplicationVersionEntity> application) {
+        this.application = application;
     }
 
     @Override
