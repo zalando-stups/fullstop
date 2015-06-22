86feab3f975fbd41d06c3e61a7f8d2a19078d49a
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-22 16:30:11 +0200

    #162 make approvals test class

--- fullstop-plugins/fullstop-registry-plugin/src/test/java/org/zalando/stups/fullstop/plugin/RegistryPluginApprovalsTest.java
+++ fullstop-plugins/fullstop-registry-plugin/src/test/java/org/zalando/stups/fullstop/plugin/RegistryPluginApprovalsTest.java
@@ -40,7 +40,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class RegistryPluginTest {
+public class RegistryPluginApprovalsTest {
 
     private static final String APPLICATION_ID = "fullstop";
 
