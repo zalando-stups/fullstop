55bdfa0080b1fa098a263a8423ece5a90ab30703
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-25 14:31:00 +0200

    #144 add tests for plugin

--- fullstop-plugins/fullstop-application-masterdata-plugin/src/test/java/org/zalando/stups/fullstop/plugin/ApplicationMasterdataPluginTest.java
+++ fullstop-plugins/fullstop-application-masterdata-plugin/src/test/java/org/zalando/stups/fullstop/plugin/ApplicationMasterdataPluginTest.java
@@ -15,6 +15,194 @@
  */
 package org.zalando.stups.fullstop.plugin;
 
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.assertj.core.util.Lists;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.zalando.stups.clients.kio.Application;
+import org.zalando.stups.clients.kio.KioOperations;
+import org.zalando.stups.clients.kio.NotFoundException;
+import org.zalando.stups.fullstop.events.Records;
+import org.zalando.stups.fullstop.events.TestCloudTrailEventData;
+import org.zalando.stups.fullstop.events.UserDataProvider;
+import org.zalando.stups.fullstop.plugin.config.ApplicationMasterdataPluginProperties;
+import org.zalando.stups.fullstop.violation.Violation;
+import org.zalando.stups.fullstop.violation.ViolationSink;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
+import com.google.common.collect.Maps;
+
 public class ApplicationMasterdataPluginTest {
-    // TODO ;)
+    private static final String APP = "test";
+
+    private ApplicationMasterdataPlugin plugin;
+
+    private KioOperations kioOperations;
+
+    private UserDataProvider userDataProvider;
+
+    private ApplicationMasterdataPluginProperties pluginProperties;
+
+    private List<NamedValidator> validators;
+
+    private ViolationSink violationSink;
+
+    private CloudTrailEvent event;
+
+    private CloudTrailEvent buildEvent() {
+        List<Map<String, Object>> records = Records.fromClasspath("/record.json");
+
+        CloudTrailEvent event = TestCloudTrailEventData.createCloudTrailEventFromMap(records.get(0));
+        return event;
+    }
+
+    private void mockUserData(boolean empty) {
+        Map<String, String> userData = Maps.newHashMap();
+        if (!empty) {
+            userData.put("application_id",
+                         APP);
+        }
+        when(userDataProvider.getUserData(any(),
+                                          any())).thenReturn(userData);
+    }
+
+    @Before
+    public void setUp() {
+        event = buildEvent();
+        kioOperations = mock(KioOperations.class);
+        userDataProvider = mock(UserDataProvider.class);
+        validators = Lists.newArrayList(new DocumentationUrlValidator(),
+                                        new ScmUrlValidator(),
+                                        new SpecificationUrlValidator());
+        violationSink = mock(ViolationSink.class);
+        pluginProperties = new ApplicationMasterdataPluginProperties();
+        pluginProperties.setValidatorsEnabled(Lists.newArrayList("scm_url",
+                                                                 "specification_url",
+                                                                 "documentation_url"));
+        plugin = new ApplicationMasterdataPlugin(kioOperations,
+                                                 userDataProvider,
+                                                 pluginProperties,
+                                                 validators,
+                                                 violationSink);
+        plugin.init();
+    }
+
+    @After
+    public void tearDown() {
+        verifyNoMoreInteractions(kioOperations,
+                                 userDataProvider,
+                                 violationSink);
+    }
+
+    @Test
+    public void shouldSupportRunEvent() {
+        assertTrue(plugin.supports(event));
+    }
+
+    @Test
+    public void shouldComplainOnException() {
+        when(userDataProvider.getUserData(any(),
+                                          any())).thenThrow(new AmazonServiceException("foo"));
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainOnEmptyUserdata() {
+        mockUserData(true);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainOnNullUserdata() {
+        when(userDataProvider.getUserData(any(),
+                                          any())).thenReturn(null);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainOnApplicationNotFound() {
+        mockUserData(false);
+        when(kioOperations.getApplicationById(APP)).thenThrow(new NotFoundException());
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(kioOperations).getApplicationById(APP);
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainAboutMissingScmUrl() {
+        mockUserData(false);
+        Application app = new Application();
+        app.setDocumentationUrl("foo");
+        app.setSpecificationUrl("bar");
+        when(kioOperations.getApplicationById(APP)).thenReturn(app);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(kioOperations).getApplicationById(APP);
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainAboutMissingDocumentationUrl() {
+        mockUserData(false);
+        Application app = new Application();
+        app.setScmUrl("foo");
+        app.setSpecificationUrl("bar");
+        when(kioOperations.getApplicationById(APP)).thenReturn(app);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(kioOperations).getApplicationById(APP);
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldComplainAboutMissingSpecificationUrl() {
+        mockUserData(false);
+        Application app = new Application();
+        app.setDocumentationUrl("foo");
+        app.setScmUrl("bar");
+        when(kioOperations.getApplicationById(APP)).thenReturn(app);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(kioOperations).getApplicationById(APP);
+        verify(violationSink).put(any(Violation.class));
+    }
+
+    @Test
+    public void shouldNotComplainInHappyCase() {
+        mockUserData(false);
+        Application app = new Application();
+        app.setDocumentationUrl("foo");
+        app.setSpecificationUrl("bar");
+        app.setScmUrl("baz");
+        when(kioOperations.getApplicationById(APP)).thenReturn(app);
+        plugin.processEvent(event);
+        verify(userDataProvider).getUserData(any(),
+                                             any());
+        verify(kioOperations).getApplicationById(APP);
+        verify(violationSink,
+               never()).put(any(Violation.class));
+    }
 }
