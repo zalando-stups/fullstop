748c36d28e24daa92954a9d5e6ccf05e749a6282
jbellmann <joerg.bellmann@zalando.de>
2015-06-30 19:29:29 +0200

    fix autoconfiguration error

--- fullstop-plugins/fullstop-instance-plugin/src/test/java/com/unknown/pkg/PluginTest.java
+++ fullstop-plugins/fullstop-instance-plugin/src/test/java/com/unknown/pkg/PluginTest.java
@@ -13,11 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import org.zalando.stups.fullstop.aws.ClientProvider;
 import org.zalando.stups.fullstop.plugin.instance.RunInstancePlugin;
+import org.zalando.stups.fullstop.violation.NoOpViolationSink;
+import org.zalando.stups.fullstop.violation.ViolationSink;
 
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEventData;
 
 /**
  * @author  jbellmann
@@ -32,8 +39,31 @@ public class PluginTest {
 
     @Test
     public void testPlugin() {
-        CloudTrailEvent event = Mockito.mock(CloudTrailEvent.class);
+        CloudTrailEvent event = getMockedEvent();
 
         Assertions.assertThat(plugin.supports(event)).isTrue();
+    }
+
+    protected CloudTrailEvent getMockedEvent() {
+        CloudTrailEvent event = Mockito.mock(CloudTrailEvent.class);
+        CloudTrailEventData eventData = Mockito.mock(CloudTrailEventData.class);
+        Mockito.when(event.getEventData()).thenReturn(eventData);
+        Mockito.when(eventData.getEventSource()).thenReturn("ec2.amazonaws.com");
+        Mockito.when(eventData.getEventName()).thenReturn("RunInstances");
+        return event;
+    }
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public ViolationSink violationSink() {
+            return new NoOpViolationSink();
+        }
+
+        @Bean
+        public ClientProvider clientProvider() {
+            return Mockito.mock(ClientProvider.class);
+        }
     }
 }
