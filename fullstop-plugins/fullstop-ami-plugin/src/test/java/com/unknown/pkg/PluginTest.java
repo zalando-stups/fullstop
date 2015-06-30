2532d03f13f8022145336d53d76e34153e170473
jbellmann <joerg.bellmann@zalando.de>
2015-06-30 19:22:58 +0200

    add needed auto-configuration

--- fullstop-plugins/fullstop-ami-plugin/src/test/java/com/unknown/pkg/PluginTest.java
+++ fullstop-plugins/fullstop-ami-plugin/src/test/java/com/unknown/pkg/PluginTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.unknown.pkg;
 
 import org.assertj.core.api.Assertions;
@@ -23,6 +38,7 @@ import org.zalando.stups.fullstop.violation.NoOpViolationSink;
 import org.zalando.stups.fullstop.violation.ViolationSink;
 
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
+import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEventData;
 
 import com.unknown.pkg.PluginTest.TestConfig;
 
@@ -31,7 +47,6 @@ import com.unknown.pkg.PluginTest.TestConfig;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = {ExampleApplication.class, TestConfig.class})
-// @IntegrationTest({ "debug=true" })
 public class PluginTest {
 
     @Autowired
@@ -39,14 +54,22 @@ public class PluginTest {
 
     @Test
     public void testPluginCreated() {
-        CloudTrailEvent event = Mockito.mock(CloudTrailEvent.class);
+        CloudTrailEvent event = getMockedEvent();
 
         Assertions.assertThat(plugin.supports(event)).isTrue();
 
     }
 
+    protected CloudTrailEvent getMockedEvent() {
+        CloudTrailEvent event = Mockito.mock(CloudTrailEvent.class);
+        CloudTrailEventData eventData = Mockito.mock(CloudTrailEventData.class);
+        Mockito.when(event.getEventData()).thenReturn(eventData);
+        Mockito.when(eventData.getEventSource()).thenReturn("ec2.amazonaws.com");
+        Mockito.when(eventData.getEventName()).thenReturn("RunInstances");
+        return event;
+    }
+
     @Configuration
-// @ComponentScan("org.zalando.stups.fullstop.plugin.ami")
     static class TestConfig {
 
         @Bean
