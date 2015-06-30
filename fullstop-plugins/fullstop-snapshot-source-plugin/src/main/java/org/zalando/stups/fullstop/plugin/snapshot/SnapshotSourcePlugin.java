ebde62ae03b99130583460f257a2c5a211e68872
jbellmann <joerg.bellmann@zalando.de>
2015-06-30 23:43:34 +0200

    #189 move into it's own package

--- fullstop-plugins/fullstop-snapshot-source-plugin/src/main/java/org/zalando/stups/fullstop/plugin/snapshot/SnapshotSourcePlugin.java
+++ fullstop-plugins/fullstop-snapshot-source-plugin/src/main/java/org/zalando/stups/fullstop/plugin/snapshot/SnapshotSourcePlugin.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.zalando.stups.fullstop.snapshot.plugin;
+package org.zalando.stups.fullstop.plugin.snapshot;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
