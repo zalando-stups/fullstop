2e8b5eb09c0b270edac030a90f2d21fbe51914fb
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-07-03 17:14:41 +0200

    wrapped client requests into Hystrix Commands + moved all management endpoints to port 7979

--- fullstop/src/main/java/org/zalando/stups/fullstop/config/HystrixConfiguration.java
+++ fullstop/src/main/java/org/zalando/stups/fullstop/config/HystrixConfiguration.java
@@ -15,22 +15,10 @@
  */
 package org.zalando.stups.fullstop.config;
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * @author jbellmann
- */
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(final WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/api/instance-logs");
-    }
-}
+@EnableHystrix
+public class HystrixConfiguration { }
---
+++ fullstop/src/main/java/org/zalando/stups/fullstop/hystrix/HystrixExceptionWebMvcHandler.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zalando.stups.fullstop.hystrix;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
+
+import com.netflix.hystrix.exception.HystrixBadRequestException;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class HystrixExceptionWebMvcHandler {
+
+    private final Logger log = getLogger(getClass());
+
+    @ResponseStatus(value = BAD_REQUEST)
+    @ExceptionHandler(HystrixBadRequestException.class) void handleBadRequestException(
+            final HystrixBadRequestException e) {
+        log.info("Request failed due to validation error.", e);
+    }
+
+    @ResponseStatus(value = SERVICE_UNAVAILABLE, reason = "Dependency unavailable")
+    @ExceptionHandler(HystrixRuntimeException.class) void handleRuntimeException(final HystrixRuntimeException e) {
+        log.warn("Request failed due to unavailable dependency.", e);
+    }
+}
