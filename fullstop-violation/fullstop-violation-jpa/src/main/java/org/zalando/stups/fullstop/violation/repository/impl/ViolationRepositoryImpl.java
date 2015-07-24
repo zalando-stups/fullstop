da68e9f33b274c470ebcbb5b3e8a6a4ccc2357f4
Gregsen <gkneitschel@gmail.com>
2015-07-24 14:36:59 +0200

    #206 - fixed broken test and license

--- fullstop-violation/fullstop-violation-jpa/src/main/java/org/zalando/stups/fullstop/violation/repository/impl/ViolationRepositoryImpl.java
+++ fullstop-violation/fullstop-violation-jpa/src/main/java/org/zalando/stups/fullstop/violation/repository/impl/ViolationRepositoryImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -52,7 +52,7 @@ public class ViolationRepositoryImpl extends QueryDslRepositorySupport implement
         QViolationEntity qViolationEntity = QViolationEntity.violationEntity;
         QViolationTypeEntity qViolationTypeEntity = QViolationTypeEntity.violationTypeEntity;
 
-        final JPQLQuery query = from(qViolationEntity).leftJoin(qViolationTypeEntity);
+        final JPQLQuery query = from(qViolationEntity).leftJoin(qViolationEntity.violationTypeEntity, qViolationTypeEntity);
 
         final List<Predicate> predicates = newArrayList();
 
@@ -87,7 +87,7 @@ public class ViolationRepositoryImpl extends QueryDslRepositorySupport implement
         }
 
         if (type != null) {
-            predicates.add(qViolationTypeEntity.eq(type));
+            predicates.add(qViolationEntity.violationTypeEntity.eq(type));
         }
 
         final long total = query.where(allOf(predicates)).count();
