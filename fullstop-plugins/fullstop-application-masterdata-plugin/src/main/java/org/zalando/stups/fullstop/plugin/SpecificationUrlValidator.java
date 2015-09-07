4ac8e4c48b4754011258ff51510d978aa81e60bd
Henning Jacobs <henning@jacobs1.de>
2015-09-07 15:52:01 +0200

    fix application properties (copy&paste error)

--- fullstop-plugins/fullstop-application-masterdata-plugin/src/main/java/org/zalando/stups/fullstop/plugin/SpecificationUrlValidator.java
+++ fullstop-plugins/fullstop-application-masterdata-plugin/src/main/java/org/zalando/stups/fullstop/plugin/SpecificationUrlValidator.java
@@ -42,7 +42,7 @@ public class SpecificationUrlValidator extends AbstractApplicationValidator {
                             "http",
                             "https"),
                     false)).validate(
-                    app.getScmUrl(),
+                    app.getSpecificationUrl(),
                     errors);
         }
         finally {
