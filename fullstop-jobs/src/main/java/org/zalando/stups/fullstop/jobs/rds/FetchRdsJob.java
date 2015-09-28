c2a952975773580a0283995ceb4f4f7bedb2eb05
Gregsen <gkneitschel@gmail.com>
2015-09-28 17:43:10 +0200

    #243 first draft for db check

---
+++ fullstop-jobs/src/main/java/org/zalando/stups/fullstop/jobs/rds/FetchRdsJob.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zalando.stups.fullstop.jobs.rds;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rds.AmazonRDSClient;
+import com.amazonaws.services.rds.model.DBInstance;
+import com.amazonaws.services.rds.model.DescribeDBInstancesRequest;
+import com.amazonaws.services.rds.model.DescribeDBInstancesResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.zalando.stups.fullstop.aws.ClientProvider;
+import org.zalando.stups.fullstop.jobs.config.JobsProperties;
+import org.zalando.stups.fullstop.teams.Account;
+import org.zalando.stups.fullstop.teams.TeamOperations;
+import org.zalando.stups.fullstop.violation.Violation;
+import org.zalando.stups.fullstop.violation.ViolationBuilder;
+import org.zalando.stups.fullstop.violation.ViolationSink;
+import org.zalando.stups.fullstop.violation.service.ViolationService;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.stream.Collectors.toList;
+import static org.zalando.stups.fullstop.violation.ViolationType.UNSECURED_ENDPOINT;
+
+/**
+ * Created by gkneitschel on 25/09/15.
+ */
+@Component
+public class FetchRdsJob {
+
+
+    private static final String EVENT_ID = "checkRdsJob";
+    private final Logger log = LoggerFactory.getLogger(FetchRdsJob.class);
+    private TeamOperations teamOperations;
+    private ClientProvider clientProvider;
+    private JobsProperties jobsProperties;
+    private ViolationService  violationService;
+    private ViolationSink violationSink;
+
+    @Autowired
+    public FetchRdsJob(TeamOperations teamOperations, ClientProvider clientProvider, JobsProperties jobsProperties, ViolationService violationService, ViolationSink violationSink) {
+        this.teamOperations = teamOperations;
+        this.clientProvider = clientProvider;
+        this.jobsProperties = jobsProperties;
+        this.violationService = violationService;
+        this.violationSink = violationSink;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("{} initialized", getClass().getSimpleName());
+    }
+
+    @Scheduled(fixedRate = 300_000)
+    public void check() {
+        for (String accountId : fetchAccountIds()) {
+            for (String region : jobsProperties.getWhitelistedRegions()) {
+                DescribeDBInstancesResult describeDBInstancesResult = getRds(accountId, region);
+                describeDBInstancesResult.getDBInstances().stream().filter(DBInstance::getPubliclyAccessible).forEach(dbInstance -> {
+//                    dbInstan
+                    writeViolation(accountId, region, "", dbInstance.getEndpoint().getAddress());
+                });
+
+            }
+
+
+        }
+
+    }
+
+    private void writeViolation(String account, String region, Object metaInfo, String canonicalHostedZoneName) {
+        ViolationBuilder violationBuilder = new ViolationBuilder();
+        Violation violation = violationBuilder.withAccountId(account)
+                .withRegion(region)
+                .withPluginFullyQualifiedClassName(FetchRdsJob.class)
+                .withType(UNSECURED_ENDPOINT)
+                .withMetaInfo(metaInfo)
+                .withEventId(EVENT_ID)
+                .withInstanceId(canonicalHostedZoneName)
+                .build();
+        violationSink.put(violation);
+    }
+    private DescribeDBInstancesResult getRds(String accountId, String region) {
+        DescribeDBInstancesRequest describeDBInstancesRequest = new DescribeDBInstancesRequest();
+        DescribeDBInstancesResult describeDBInstancesResult;
+
+            AmazonRDSClient amazonRDSClient = clientProvider.getClient(AmazonRDSClient.class, accountId,
+                    Region.getRegion(Regions.fromName(region)));
+            describeDBInstancesResult = amazonRDSClient.describeDBInstances(describeDBInstancesRequest);
+
+
+        return describeDBInstancesResult;
+    }
+
+    private List<String> fetchAccountIds() {
+        List<String> accountIds = newArrayList();
+        List<Account> accounts = teamOperations.getAccounts();
+        accountIds.addAll(accounts.stream().map(Account::getId).collect(toList()));
+        return accountIds;
+
+    }
+}
