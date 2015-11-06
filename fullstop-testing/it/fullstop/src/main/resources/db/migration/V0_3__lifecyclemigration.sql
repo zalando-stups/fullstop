9257302d103d6a517725337c59bf137257edefcf
Michele Randi <michele.randi@gmail.com>
2015-11-06 13:27:23 +0100

    #256 add flyway

--- fullstop-testing/it/fullstop/src/main/resources/db/migration/V0_3__lifecyclemigration.sql
+++
@@ -1,6 +0,0 @@
---SET ROLE TO fullstop_app;
-
-
-ALTER TABLE fullstop_data.lifecycle ADD COLUMN account_id TEXT;
-
-ALTER TABLE fullstop_data.lifecycle ADD COLUMN image_id TEXT, ADD COLUMN image_name TEXT;
