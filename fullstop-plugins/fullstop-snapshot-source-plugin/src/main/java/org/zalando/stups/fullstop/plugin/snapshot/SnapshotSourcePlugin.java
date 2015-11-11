3d7acd16f6c5452f696945ac31594d3a0c975078
Gregsen <gkneitschel@gmail.com>
2015-11-11 16:49:03 +0100

    #272 clean up violation type messages and add serverity and title

--- fullstop-plugins/fullstop-snapshot-source-plugin/src/main/java/org/zalando/stups/fullstop/plugin/snapshot/SnapshotSourcePlugin.java
+++ fullstop-plugins/fullstop-snapshot-source-plugin/src/main/java/org/zalando/stups/fullstop/plugin/snapshot/SnapshotSourcePlugin.java
@@ -76,7 +76,7 @@ public class SnapshotSourcePlugin extends AbstractFullstopPlugin {
                 return;
             }
 
-            if (userData == null) {
+            if (userData == null) {//TODO: taupage only!
                 violationSink.put(violationFor(event).withInstanceId(id).withPluginFullyQualifiedClassName(
                                           SnapshotSourcePlugin.class).withType(MISSING_USER_DATA).build());
                 return;
