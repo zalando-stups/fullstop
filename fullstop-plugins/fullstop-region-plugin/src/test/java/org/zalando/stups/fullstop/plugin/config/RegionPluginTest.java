13ab09da8ca71a5ad6d672f919cb462fa415c709
Christian Lohmann <christian.lohmann@zalando.de>
2015-11-30 17:07:47 +0100

    adapted test

--- fullstop-plugins/fullstop-region-plugin/src/test/java/org/zalando/stups/fullstop/plugin/config/RegionPluginTest.java
+++ fullstop-plugins/fullstop-region-plugin/src/test/java/org/zalando/stups/fullstop/plugin/config/RegionPluginTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,22 +15,29 @@
  */
 package org.zalando.stups.fullstop.plugin.config;
 
-import com.amazonaws.services.cloudtrail.processinglibrary.exceptions.CallbackException;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.zalando.stups.fullstop.events.TestCloudTrailEventSerializer.createCloudTrailEvent;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.zalando.stups.fullstop.aws.ClientProvider;
 import org.zalando.stups.fullstop.plugin.EC2InstanceContext;
 import org.zalando.stups.fullstop.plugin.EC2InstanceContextProvider;
 import org.zalando.stups.fullstop.plugin.LocalPluginProcessor;
 import org.zalando.stups.fullstop.plugin.RegionPlugin;
+import org.zalando.stups.fullstop.plugin.impl.EC2InstanceContextProviderImpl;
+import org.zalando.stups.fullstop.plugin.provider.AmiIdProvider;
+import org.zalando.stups.fullstop.plugin.provider.AmiNameProvider;
 import org.zalando.stups.fullstop.violation.SystemOutViolationSink;
 import org.zalando.stups.fullstop.violation.Violation;
-import org.zalando.stups.fullstop.violation.ViolationBuilder;
 import org.zalando.stups.fullstop.violation.ViolationSink;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
-import static org.zalando.stups.fullstop.events.TestCloudTrailEventSerializer.createCloudTrailEvent;
+import com.amazonaws.services.cloudtrail.processinglibrary.exceptions.CallbackException;
 
 /**
  * @author jbellmann
@@ -40,17 +47,19 @@ public class RegionPluginTest {
     private ViolationSink violationSink = new SystemOutViolationSink();
     private RegionPlugin plugin;
     private RegionPluginProperties regionPluginProperties;
-    private EC2InstanceContext instanceContext;
+    private EC2InstanceContextProviderImpl contextProvider;
 
     @Before
     public void setUp() {
-        final EC2InstanceContextProvider contextProvider = mock(EC2InstanceContextProvider.class);
+        final ClientProvider clientProvider = mock(ClientProvider.class);
+        final AmiIdProvider amiIdProvider = mock(AmiIdProvider.class);
+        final AmiNameProvider amiNameProvider = mock(AmiNameProvider.class);
+        contextProvider = new EC2InstanceContextProviderImpl(clientProvider,
+                amiIdProvider,
+                amiNameProvider);
         violationSink = Mockito.spy(violationSink);
-//        instanceContext = mock(EC2InstanceContext.class);
         regionPluginProperties = new RegionPluginProperties();
         plugin = new RegionPlugin(contextProvider, violationSink, regionPluginProperties);
-
-//        when(instanceContext.violation()).thenReturn(new ViolationBuilder());
     }
 
     @Test
@@ -69,7 +78,6 @@ public class RegionPluginTest {
 
     @Test
     public void testWithLocalPluginProcessor() throws CallbackException {
-        final EC2InstanceContextProvider contextProvider = mock(EC2InstanceContextProvider.class);
         RegionPlugin plugin = new RegionPlugin(contextProvider, violationSink, regionPluginProperties);
         LocalPluginProcessor lpp = new LocalPluginProcessor(plugin);
         lpp.processEvents(getClass().getResourceAsStream("/record-run.json"));
