d46238ba599b00127a4926df5a2bba2d0866b5b7
Andre Hartmann <andre.hartmann@zalando.de>
2015-12-01 12:34:21 +0100

    #296 tested ApplicationMasterdataPlugin and config

--- fullstop-plugins/fullstop-application-masterdata-plugin/src/test/java/org/zalando/stups/fullstop/plugin/ApplicationMasterdataPluginTest.java
+++ fullstop-plugins/fullstop-application-masterdata-plugin/src/test/java/org/zalando/stups/fullstop/plugin/ApplicationMasterdataPluginTest.java
@@ -18,10 +18,9 @@ package org.zalando.stups.fullstop.plugin;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.zalando.stups.clients.kio.Application;
 import org.zalando.stups.fullstop.violation.ViolationBuilder;
-import org.zalando.stups.fullstop.violation.ViolationMatchers;
 import org.zalando.stups.fullstop.violation.ViolationSink;
-import org.zalando.stups.fullstop.violation.ViolationType;
 
 import java.util.Optional;
 
@@ -29,7 +28,7 @@ import static java.util.Optional.empty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.zalando.stups.fullstop.violation.ViolationMatchers.hasType;
-import static org.zalando.stups.fullstop.violation.ViolationType.APPLICATION_NOT_PRESENT_IN_KIO;
+import static org.zalando.stups.fullstop.violation.ViolationType.*;
 
 public class ApplicationMasterdataPluginTest {
 
@@ -38,9 +37,11 @@ public class ApplicationMasterdataPluginTest {
     private EC2InstanceContextProvider mockContextProvider;
     private ViolationSink mockViolationSink;
     private EC2InstanceContext mockContext;
+    private Application kioApp;
 
     @Before
     public void setUp() throws Exception {
+        kioApp = new Application();
         mockContextProvider = mock(EC2InstanceContextProvider.class);
         mockViolationSink = mock(ViolationSink.class);
         mockContext = mock(EC2InstanceContext.class);
@@ -84,5 +85,34 @@ public class ApplicationMasterdataPluginTest {
         verify(mockContext).getKioApplication();
         verify(mockContext).violation();
         verify(mockViolationSink).put(argThat(hasType(APPLICATION_NOT_PRESENT_IN_KIO)));
+    }
+
+    @Test
+    public void testMissingUrl() throws Exception {
+        when(mockContext.getApplicationId()).thenReturn(Optional.of("my-app"));
+        when(mockContext.getKioApplication()).thenReturn(Optional.of(kioApp));
+        kioApp.setSpecificationType(null);
+        kioApp.setSpecificationUrl(" ");
+
+        plugin.process(mockContext);
+
+        verify(mockContext).getApplicationId();
+        verify(mockContext).getKioApplication();
+        verify(mockContext, times(2)).violation();
+        verify(mockViolationSink).put(argThat(hasType(SPEC_TYPE_IS_MISSING_IN_KIO)));
+        verify(mockViolationSink).put(argThat(hasType(SPEC_URL_IS_MISSING_IN_KIO)));
+    }
+
+    @Test
+    public void testHappyCase() throws Exception {
+        when(mockContext.getApplicationId()).thenReturn(Optional.of("my-app"));
+        when(mockContext.getKioApplication()).thenReturn(Optional.of(kioApp));
+        kioApp.setSpecificationType("Github Issues");
+        kioApp.setSpecificationUrl("https://github.com/zalando-stups/fullstop/issues");
+
+        plugin.process(mockContext);
+
+        verify(mockContext).getApplicationId();
+        verify(mockContext).getKioApplication();
     }
 }
---
+++ fullstop-plugins/fullstop-application-masterdata-plugin/src/test/java/org/zalando/stups/fullstop/plugin/config/ApplicationMasterdataPluginAutoConfigurationTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2015 Zalando SE (http://tech.zalando.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.zalando.stups.fullstop.plugin.config;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zalando.stups.fullstop.plugin.ApplicationMasterdataPlugin;
+import org.zalando.stups.fullstop.plugin.EC2InstanceContextProvider;
+import org.zalando.stups.fullstop.violation.ViolationSink;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class ApplicationMasterdataPluginAutoConfigurationTest {
+
+    @Autowired(required = false)
+    private ApplicationMasterdataPlugin plugin;
+
+    @Test
+    public void testApplicationMasterdataPlugin() throws Exception {
+        assertThat(plugin).isNotNull();
+    }
+
+    @Configuration
+    @EnableAutoConfiguration
+    static class TestConfig {
+
+        @Bean
+        EC2InstanceContextProvider contextProvider(){
+            return mock(EC2InstanceContextProvider.class);
+        }
+
+        @Bean
+        ViolationSink violationSink(){
+            return mock(ViolationSink.class);
+        }
+    }
+}
