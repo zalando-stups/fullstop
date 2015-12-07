28a051ade54e8a161d23af1b09a14ef597b15e15
Gregsen <gkneitschel@gmail.com>
2015-12-07 18:07:52 +0100

    #296 add tests for PieroneImageImpl

---
+++ fullstop-plugins/fullstop-instance-plugin-support/src/test/java/org/zalando/stups/fullstop/plugin/provider/impl/PieroneImageTest.java
@@ -0,0 +1,63 @@
+package org.zalando.stups.fullstop.plugin.provider.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static java.nio.file.Paths.get;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PieroneImageTest {
+
+    public static final String REPOSITORY = "pierone.example.org";
+    public static final String TEAM = "team";
+    public static final String ARTIFACT = "artifact";
+    public static final String TAG = "0.1.0.5.0-7.8";
+    private PieroneImage pieroneImage;
+
+    @Before
+    public void setUp() throws Exception {
+        pieroneImage = new PieroneImage(REPOSITORY, TEAM, ARTIFACT, TAG);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testTryParse() throws Exception {
+        Optional<PieroneImage> result = PieroneImage.tryParse(get(REPOSITORY, TEAM, ARTIFACT).toString()+":"+TAG);
+
+        assertThat(result).isPresent();
+
+        assertThat(result.get().getRepository()).isEqualTo(REPOSITORY);
+        assertThat(result.get().getTeam()).isEqualTo(TEAM);
+        assertThat(result.get().getArtifact()).isEqualTo(ARTIFACT);
+        assertThat(result.get().getTag()).isEqualTo(TAG);
+
+
+    }
+
+    @Test
+    public void testGetTag() throws Exception {
+        assertThat(pieroneImage.getTag()).isEqualTo(TAG);
+    }
+
+    @Test
+    public void testGetArtifact() throws Exception {
+        assertThat(pieroneImage.getArtifact()).isEqualTo(ARTIFACT);
+    }
+
+    @Test
+    public void testGetTeam() throws Exception {
+        assertThat(pieroneImage.getTeam()).isEqualTo(TEAM);
+    }
+
+    @Test
+    public void testGetRepository() throws Exception {
+        assertThat(pieroneImage.getRepository()).isEqualTo(REPOSITORY);
+    }
+}
