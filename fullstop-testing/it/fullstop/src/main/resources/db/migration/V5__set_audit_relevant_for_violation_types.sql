41f754843939c1fc3a4459ecc88d4895cfb60947
Christian Lohmann <christian.lohmann@zalando.de>
2015-12-11 13:36:29 +0100

    #312 added semicolon

--- fullstop-testing/it/fullstop/src/main/resources/db/migration/V5__set_audit_relevant_for_violation_types.sql
+++ fullstop-testing/it/fullstop/src/main/resources/db/migration/V5__set_audit_relevant_for_violation_types.sql
@@ -1,4 +1,4 @@
 UPDATE fullstop_data.violation_type SET is_audit_relevant = false WHERE is_audit_relevant IS NULL;
 ALTER TABLE fullstop_data.violation_type
   ALTER COLUMN is_audit_relevant SET NOT NULL,
-  ALTER COLUMN is_audit_relevant SET DEFAULT false
+  ALTER COLUMN is_audit_relevant SET DEFAULT false;
