d3d8fa8b03138223c8cbbeb0b517ee7de34c59bd
Andre Hartmann <andre.hartmann@zalando.de>
2016-01-20 16:14:26 +0100

    #252 implemented ScmRepositoryPlugin via EC2InstanceContext

--- fullstop-plugins/fullstop-scm-repository-plugin/src/main/java/org/zalando/stups/fullstop/plugin/scm/ScmRepositoryPlugin.java
+++ fullstop-plugins/fullstop-scm-repository-plugin/src/main/java/org/zalando/stups/fullstop/plugin/scm/ScmRepositoryPlugin.java
@@ -1,188 +1,118 @@
 package org.zalando.stups.fullstop.plugin.scm;
 
-import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
-import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEventData;
 import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.yaml.snakeyaml.Yaml;
 import org.zalando.kontrolletti.KontrollettiOperations;
-import org.zalando.kontrolletti.resources.Repository;
 import org.zalando.stups.clients.kio.Application;
-import org.zalando.stups.clients.kio.KioOperations;
-import org.zalando.stups.clients.kio.NotFoundException;
-import org.zalando.stups.fullstop.events.UserDataProvider;
-import org.zalando.stups.fullstop.plugin.AbstractFullstopPlugin;
+import org.zalando.stups.fullstop.plugin.AbstractEC2InstancePlugin;
+import org.zalando.stups.fullstop.plugin.EC2InstanceContext;
+import org.zalando.stups.fullstop.plugin.EC2InstanceContextProvider;
 import org.zalando.stups.fullstop.violation.ViolationSink;
-import org.zalando.stups.pierone.client.PieroneOperations;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import static java.util.Collections.singletonMap;
+import static java.util.function.Predicate.isEqual;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.slf4j.LoggerFactory.getLogger;
-import static org.zalando.stups.fullstop.events.CloudTrailEventSupport.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.zalando.stups.fullstop.violation.ViolationType.*;
 
 @Component
-public class ScmRepositoryPlugin extends AbstractFullstopPlugin {
-
-    private static final String EVENT_NAME = "RunInstances";
-
-    private static final String EC2_SOURCE_EVENTS = "ec2.amazonaws.com";
-
-    private static final String SOURCE = "source";
-
-    private static final String APPLICATION_ID = "application_id";
-
-    private static final Pattern DOCKER_SOURCE_PATTERN = Pattern.compile("^(?>.*/)?(.+):(.+)$");
+public class ScmRepositoryPlugin extends AbstractEC2InstancePlugin {
 
     private static final String URL = "url";
 
     private final Logger log = getLogger(getClass());
 
-    private final KioOperations kioOperations;
-
-    private final PieroneOperations pieroneOperations;
-
     private final KontrollettiOperations kontrollettiOperations;
-
-    private final UserDataProvider userDataProvider;
 
     private final ViolationSink violationSink;
 
     @Autowired
     public ScmRepositoryPlugin(
-            final ViolationSink violationSink,
-            final KioOperations kioOperations,
-            final PieroneOperations pieroneOperations,
+            final EC2InstanceContextProvider contextProvider,
             final KontrollettiOperations kontrollettiOperations,
-            final UserDataProvider userDataProvider) {
-        this.kioOperations = kioOperations;
-        this.pieroneOperations = pieroneOperations;
+            final ViolationSink violationSink) {
+        super(contextProvider);
         this.kontrollettiOperations = kontrollettiOperations;
-        this.userDataProvider = userDataProvider;
         this.violationSink = violationSink;
     }
 
     @Override
-    public boolean supports(final CloudTrailEvent event) {
-        final CloudTrailEventData cloudTrailEventData = event.getEventData();
-        final String eventSource = cloudTrailEventData.getEventSource();
-        final String eventName = cloudTrailEventData.getEventName();
-
-        return eventSource.equals(EC2_SOURCE_EVENTS) && eventName.equals(EVENT_NAME);
+    protected Predicate<? super String> supportsEventName() {
+        return isEqual(RUN_INSTANCES).or(isEqual(START_INSTANCES));
     }
 
     @Override
-    public void processEvent(final CloudTrailEvent event) {
-        for (final String instanceId : getInstanceIds(event)) {
-            final Map userData = userDataProvider.getUserData(getAccountId(event), getRegion(event), instanceId);
+    protected void process(EC2InstanceContext context) {
+        final Optional<Application> optionalKioApp = context.getKioApplication();
+        if (!optionalKioApp.isPresent()) {
+            log.warn("Missing Kio App for {}. Skip ScmRepositoryPlugin.", context);
+            return;
+        }
 
-            if (userData == null) {
-                log.warn(
-                        "No userData available for EC2 instance '{}'. Skip execution of ScmRepositoryPlugin!",
-                        instanceId);
-                return;
-            }
+        final Application app = optionalKioApp.get();
+        final String kioScmUrl = app.getScmUrl();
+        if (isBlank(kioScmUrl)) {
+            violationSink.put(
+                    context.violation()
+                            .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
+                            .withType(SCM_URL_IS_MISSING_IN_KIO)
+                            .withMetaInfo(singletonMap("application_id", app.getId()))
+                            .build());
+            return;
+        }
 
-            final String source = (String) userData.get(SOURCE);
-            if (isBlank(source)) {
-                log.warn(
-                        "'Source' field is missing in userData of EC2 instance '{}'. Skip execution of ScmRepositoryPlugin!",
-                        instanceId);
-                return;
-            }
+        final Optional<Map<String, String>> optionalScmSource = context.getScmSource();
+        if (!optionalScmSource.isPresent()) {
+            log.warn("Missing Scm source for {}. Skip ScmRepositoryPlugin.", context);
+            return;
+        }
 
-            final Matcher sourceMatcher = DOCKER_SOURCE_PATTERN.matcher(source);
-            if (!sourceMatcher.matches()) {
-                log.warn(
-                        "'{}' is not a valid docker source (or my regex is bulls**t). Skip execution of ScmRepositoryPlugin!",
-                        source);
-                return;
-            }
+        final Map<String, String> scmSource = optionalScmSource.get();
+        final String scmSourceUrl = scmSource.get(URL);
+        if (isBlank(scmSourceUrl)) {
+            violationSink.put(
+                    context.violation()
+                            .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
+                            .withType(SCM_URL_IS_MISSING_IN_SCM_SOURCE_JSON)
+                            .withMetaInfo(ImmutableMap.of(
+                                    "deployment_artifact", context.getSource().orElse(""),
+                                    "scm_source", new Yaml().dump(scmSource)))
+                            .build());
+            return;
+        }
 
-            final String artifact = sourceMatcher.group(1);
-            final String tag = sourceMatcher.group(2);
+        final String normalizedKioScmUrl = kontrollettiOperations.normalizeRepositoryUrl(kioScmUrl);
+        final String normalizedScmSourceUrl = kontrollettiOperations.normalizeRepositoryUrl(scmSourceUrl);
 
-            final String applicationId = (String) userData.get(APPLICATION_ID);
-            if (isBlank(applicationId)) {
-                log.warn(
-                        "'application_id' is missing in userData of EC2 instance {}. Skip execution of ScmRepositoryPlugin!",
-                        instanceId);
-                return;
-            }
+        if (!Objects.equals(normalizedKioScmUrl, normalizedScmSourceUrl)) {
+            violationSink.put(
+                    context.violation()
+                            .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
+                            .withType(SCM_URL_NOT_MATCH_WITH_KIO)
+                            .withMetaInfo(ImmutableMap.of(
+                                    "normalized_scm_source_url",
+                                    normalizedScmSourceUrl,
+                                    "normalized_kio_scm_url",
+                                    normalizedKioScmUrl)).build());
+            return;
+        }
 
-            final Application app;
-            try {
-                app = kioOperations.getApplicationById(applicationId);
-            }
-            catch (NotFoundException e) {
-                log.warn("app '{}' does not exist in Kio. Skip execution of ScmRepositoryPlugin!", applicationId);
-                return;
-            }
-
-            final String team = app.getTeamId();
-            final String kioScmUrl = app.getScmUrl();
-            if (isBlank(kioScmUrl)) {
+        try {
+            kontrollettiOperations.getRepository(normalizedScmSourceUrl);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == NOT_FOUND) {
                 violationSink.put(
-                        violationFor(event)
-                                .withInstanceId(instanceId)
-                                .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
-                                .withType(SCM_URL_IS_MISSING_IN_KIO)
-                                .withMetaInfo(ImmutableMap.of(
-                                        "team", team,
-                                        "artifact", artifact,
-                                        "version", tag))
-                                .build());
-                return;
-            }
-
-            final Map<String, String> scmSource = pieroneOperations.getScmSource(team, artifact, tag);
-            if (scmSource == null) {
-                log.warn("scm-source.json is missing. Skip execution of ScmRepositoryPlugin!");
-                return;
-            }
-
-            final String scmSourceUrl = scmSource.get(URL);
-            if (isBlank(scmSourceUrl)) {
-                violationSink.put(
-                        violationFor(event)
-                                .withInstanceId(instanceId)
-                                .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
-                                .withType(SCM_URL_IS_MISSING_IN_SCM_SOURCE_JSON)
-                                .withMetaInfo(ImmutableMap.of(
-                                        "team", team,
-                                        "artifact", artifact,
-                                        "version", tag,
-                                        "scm_source", scmSource))
-                                .build());
-                return;
-            }
-
-            final String normalizedKioScmUrl = kontrollettiOperations.normalizeRepositoryUrl(kioScmUrl);
-            final String normalizedScmSourceUrl = kontrollettiOperations.normalizeRepositoryUrl(scmSourceUrl);
-
-            if (!Objects.equals(normalizedKioScmUrl, normalizedScmSourceUrl)) {
-                violationSink.put(
-                        violationFor(event).withInstanceId(instanceId).withPluginFullyQualifiedClassName(
-                                ScmRepositoryPlugin.class).withType(SCM_URL_NOT_MATCH_WITH_KIO).withMetaInfo(
-                                ImmutableMap.of(
-                                        "normalized_scm_source_url",
-                                        normalizedScmSourceUrl,
-                                        "normalized_kio_scm_url",
-                                        normalizedKioScmUrl)).build());
-                return;
-            }
-
-            final Repository repository = kontrollettiOperations.getRepository(normalizedScmSourceUrl);
-            if (repository == null) {
-                violationSink.put(
-                        violationFor(event)
-                                .withInstanceId(instanceId)
+                        context.violation()
                                 .withPluginFullyQualifiedClassName(ScmRepositoryPlugin.class)
                                 .withType(ILLEGAL_SCM_REPOSITORY)
                                 .withMetaInfo(singletonMap("normalized_repository_url", normalizedScmSourceUrl))
