2e782c926b298272eae3276b433e91b28a8faa19
Gregsen <gkneitschel@gmail.com>
2016-02-02 14:17:37 +0100

    #351 added more tests

--- fullstop-plugins/fullstop-save-securitygroups-plugin/src/test/java/org/zalando/stups/fullstop/plugin/SecurityGroupProviderTest.java
+++ fullstop-plugins/fullstop-save-securitygroups-plugin/src/test/java/org/zalando/stups/fullstop/plugin/SecurityGroupProviderTest.java
@@ -1,9 +1,13 @@
 package org.zalando.stups.fullstop.plugin;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsRequest;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsResult;
 import com.google.common.collect.Lists;
+import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,18 +19,18 @@ public class SecurityGroupProviderTest {
 
     private ClientProvider clientProviderMock;
     private SecurityGroupProvider securityGroupProvider;
+    private AmazonEC2Client amazonEC2ClientMock;
     private static Region REGION = Region.getRegion(Regions.EU_WEST_1);
 
     @Before
     public void setUp() throws Exception {
         clientProviderMock = mock(ClientProvider.class);
-
-
+        amazonEC2ClientMock = mock(AmazonEC2Client.class);
     }
 
     @After
     public void tearDown() throws Exception {
-        verifyNoMoreInteractions(clientProviderMock);
+        verifyNoMoreInteractions(clientProviderMock, amazonEC2ClientMock);
     }
 
     @Test(expected = RuntimeException.class)
@@ -43,7 +47,37 @@ public class SecurityGroupProviderTest {
     }
 
     @Test
-    public void testGetSecurityGroup() throws Exception {
+    public void testAmazonException(){
+        AmazonServiceException amazonServiceException = new AmazonServiceException("");
+        amazonServiceException.setErrorCode("InvalidGroup.NotFound");
 
+        when(clientProviderMock.getClient(any(), anyString(), any(Region.class))).thenReturn(amazonEC2ClientMock);
+        when(amazonEC2ClientMock.describeSecurityGroups(any(DescribeSecurityGroupsRequest.class))).thenThrow(amazonServiceException);
+
+        securityGroupProvider = new SecurityGroupProvider(clientProviderMock);
+        String securityGroup = securityGroupProvider.getSecurityGroup(Lists.newArrayList("sg.1234"), REGION, "9876");
+
+        Assertions.assertThat(securityGroup).isEqualTo(null);
+
+        verify(clientProviderMock).getClient(any(), anyString(), any(Region.class));
+        verify(amazonEC2ClientMock).describeSecurityGroups(any(DescribeSecurityGroupsRequest.class));
+    }
+
+
+    @Test
+    public void testJsonException(){
+        DescribeSecurityGroupsResult mockResult = spy(new DescribeSecurityGroupsResult());
+
+        when(clientProviderMock.getClient(any(), anyString(), any(Region.class))).thenReturn(amazonEC2ClientMock);
+        when(mockResult.getSecurityGroups()).thenThrow(new IllegalStateException());
+        when(amazonEC2ClientMock.describeSecurityGroups(any(DescribeSecurityGroupsRequest.class))).thenReturn(mockResult);
+
+        securityGroupProvider = new SecurityGroupProvider(clientProviderMock);
+        String securityGroup = securityGroupProvider.getSecurityGroup(Lists.newArrayList("sg.1234"), REGION, "9876");
+
+        Assertions.assertThat(securityGroup).isEqualTo(null);
+
+        verify(clientProviderMock).getClient(any(), anyString(), any(Region.class));
+        verify(amazonEC2ClientMock).describeSecurityGroups(any(DescribeSecurityGroupsRequest.class));
     }
 }
