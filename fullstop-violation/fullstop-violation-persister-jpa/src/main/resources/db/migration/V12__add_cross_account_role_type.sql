cf4ad606f8dab74a6ac1eb1cc93f5a019cb22730
André Hartmann <andre.hartmann206@gmail.com>
2016-04-04 15:20:24 +0200

    #372 fix typo

--- fullstop-violation/fullstop-violation-persister-jpa/src/main/resources/db/migration/V12__add_cross_account_role_type.sql
+++ fullstop-violation/fullstop-violation-persister-jpa/src/main/resources/db/migration/V12__add_cross_account_role_type.sql
@@ -8,7 +8,7 @@ INSERT INTO fullstop_data.violation_type (
   created_by,
   version)
 VALUES ('CROSS_ACCOUNT_ROLE',
-        'Your account has an IAM policy that allows access from another account. Cross account access is should be avoided',
+        'Your account has an IAM policy that allows access from another account. Cross account access should be avoided',
         0,
         4,
         'Cross account role',
