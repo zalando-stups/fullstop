c7e6f08f491b8d4bfb869a7e3827465040718b58
Andre Hartmann <andre.hartmann@zalando.de>
2016-07-01 17:30:12 +0200

    #457 use JsonDiff to compare roloe policies

--- fullstop-plugins/fullstop-unapproved-services-and-role-plugin/src/test/java/org/zalando/stups/fullstop/plugin/unapproved/UnapprovedServicesAndRolePluginTest.java
+++ fullstop-plugins/fullstop-unapproved-services-and-role-plugin/src/test/java/org/zalando/stups/fullstop/plugin/unapproved/UnapprovedServicesAndRolePluginTest.java
@@ -1,7 +1,9 @@
 package org.zalando.stups.fullstop.plugin.unapproved;
 
 import com.amazonaws.services.cloudtrail.processinglibrary.model.CloudTrailEvent;
-import org.assertj.core.api.Assertions;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.fabric8.zjsonpatch.JsonDiff;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,13 +12,22 @@ import org.zalando.stups.fullstop.violation.Violation;
 import org.zalando.stups.fullstop.violation.ViolationSink;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 import static org.zalando.stups.fullstop.events.TestCloudTrailEventSerializer.createCloudTrailEvent;
 
 /**
  * Created by mrandi.
  */
 public class UnapprovedServicesAndRolePluginTest {
+
+    private static final String POLICY_1 = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"\",\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::123:xxx-provider/Shibboleth\"},\"Action\":\"sts:AssumeRoleWithxxx\",\"Condition\":{\"StringEquals\":{\"xxx:au\":\"https://signin.aws.amazon.com/xxx\"}}}]}";
+    private static final String POLICY_2 = "{\"Statement\":[{\"Sid\":\"\",\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::123:xxx-provider/Shibboleth\"},\"Action\":\"sts:AssumeRoleWithxxx\",\"Condition\":{\"StringEquals\":{\"xxx:au\":\"https://signin.aws.amazon.com/xxx\"}}}],\"Version\":\"2012-10-17\"}";
 
     private PolicyProvider policyProviderMock;
 
@@ -31,7 +42,7 @@ public class UnapprovedServicesAndRolePluginTest {
     @Before
     public void setUp() throws Exception {
         policyProviderMock = mock(PolicyProvider.class);
-        violationSinkMock = mock(ViolationSink.class);
+        violationSinkMock = mock(ViolationSink.class, withSettings().verboseLogging());
         policyTemplatesProviderMock = mock(PolicyTemplatesProvider.class);
 
         event = createCloudTrailEvent("/record.json");
@@ -57,7 +68,7 @@ public class UnapprovedServicesAndRolePluginTest {
                         "mint-worker-b17-AppServerRole-W5WX8WewafwO2MEWZ"));
 
         final boolean result = plugin.supports(event);
-        Assertions.assertThat(result).isTrue();
+        assertThat(result).isTrue();
 
         verify(policyTemplatesProviderMock).getPolicyTemplateNames();
     }
@@ -79,14 +90,24 @@ public class UnapprovedServicesAndRolePluginTest {
     @Test
     public void testProcessEvent2() throws Exception {
 
-        when(policyProviderMock.getPolicy(any(), any(), any())).thenReturn(
-                "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Sid\":\"\",\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::123:xxx-provider/Shibboleth\"},\"Action\":\"sts:AssumeRoleWithxxx\",\"Condition\":{\"StringEquals\":{\"xxx:au\":\"https://signin.aws.amazon.com/xxx\"}}}]}");
-        when(policyTemplatesProviderMock.getPolicyTemplate(any())).thenReturn(
-                "{\"Statement\":[{\"Sid\":\"\",\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::123:xxx-provider/Shibboleth\"},\"Action\":\"sts:AssumeRoleWithxxx\",\"Condition\":{\"StringEquals\":{\"xxx:au\":\"https://signin.aws.amazon.com/xxx\"}}}],\"Version\":\"2012-10-17\"}");
+        when(policyProviderMock.getPolicy(any(), any(), any())).thenReturn(POLICY_1);
+        when(policyTemplatesProviderMock.getPolicyTemplate(any())).thenReturn(POLICY_2);
 
         plugin.processEvent(event);
 
         verify(policyProviderMock).getPolicy(any(), any(), any());
         verify(policyTemplatesProviderMock).getPolicyTemplate(any());
+    }
+
+    @Test
+    public void testJsonDiff() throws Exception {
+        final ObjectMapper om = new ObjectMapper();
+
+        // Policy 1 and 2 are identical. The json has just a different order.
+        final JsonNode policy1 = om.readTree(POLICY_1);
+        final JsonNode policy2 = om.readTree(POLICY_2);
+        final JsonNode diff = JsonDiff.asJson(policy1, policy2);
+
+        assertThat(diff).isEqualTo(om.readTree("[]"));
     }
 }
