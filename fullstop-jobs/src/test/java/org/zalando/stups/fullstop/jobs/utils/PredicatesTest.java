26785dee87b4a714d5daa4c076371457ea843dd8
Andre Hartmann <andre.hartmann@zalando.de>
2017-07-14 18:09:59 +0200

    added more test cases

--- fullstop-jobs/src/test/java/org/zalando/stups/fullstop/jobs/utils/PredicatesTest.java
+++ fullstop-jobs/src/test/java/org/zalando/stups/fullstop/jobs/utils/PredicatesTest.java
@@ -2,6 +2,7 @@ package org.zalando.stups.fullstop.jobs.utils;
 
 import com.amazonaws.services.ec2.model.IpPermission;
 import com.amazonaws.services.ec2.model.IpRange;
+import com.amazonaws.services.ec2.model.Ipv6Range;
 import com.amazonaws.services.ec2.model.SecurityGroup;
 import com.amazonaws.services.ec2.model.UserIdGroupPair;
 import org.junit.Before;
@@ -11,48 +12,211 @@ import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PredicatesTest {
 
     private Predicate<SecurityGroup> pred;
+    private SecurityGroup securityGroup;
 
     @Before
     public void setUp() throws Exception {
         final Set<Integer> allowedPorts = Stream.of(22, 80, 443).collect(toSet());
         pred = Predicates.securityGroupExposesNotAllowedPorts(allowedPorts);
+
+        securityGroup = new SecurityGroup();
+        securityGroup.withGroupId("sg-12345678");
+        securityGroup.withGroupName("TestSecurityGroup");
+        securityGroup.withOwnerId("111222333444");
+        securityGroup.withIpPermissions(emptyList());
     }
 
     @Test
-    public void securityGroupExposesNotAllowedPorts() throws Exception {
-        final SecurityGroup securityGroup = new SecurityGroup()
-                .withGroupId("sg-12345678")
-                .withGroupName("TestSecurityGroup")
-                .withOwnerId("111222333444")
-                .withIpPermissions(
-                        // a tcp port opened to some internal network only
-                        new IpPermission()
-                                .withFromPort(9100)
-                                .withToPort(9100)
-                                .withIpv4Ranges(new IpRange().withCidrIp("172.31.0.0/16"))
-                                .withIpProtocol("tcp"),
-                        // ICMP
-                        new IpPermission()
-                                .withFromPort(-1)
-                                .withToPort(-1)
-                                .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0"))
-                                .withIpProtocol("icmp"),
-                        // All traffic accessible from some internal security groups
-                        new IpPermission()
-                                .withIpProtocol("-1")
-                                .withUserIdGroupPairs(
-                                        new UserIdGroupPair().withUserId("111222333444").withGroupId("sg-11223344"),
-                                        new UserIdGroupPair().withUserId("111222333444").withGroupId("sg-55667788"))
-                        // TODO find more examples
-                );
+    public void testAllTrafficFromSecurityGroups() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("-1")
+                        .withUserIdGroupPairs(
+                                new UserIdGroupPair().withUserId("111222333444").withGroupId("sg-11223344")));
 
-        // rejecting meets, that there is no open public port found
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllTrafficFromPrivateNetworks() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("-1")
+                        .withIpv4Ranges(
+                                new IpRange().withCidrIp("172.31.0.0/16")
+                                // todo add more private ip range examples
+                        ));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllTrafficFromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("-1")
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllTrafficFromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("-1")
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllTcpFromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(0)
+                        .withToPort(65535)
+                        .withIpProtocol("tcp")
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllTcpFromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(0)
+                        .withToPort(65535)
+                        .withIpProtocol("tcp")
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllUDPFromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("udp")
+                        .withFromPort(0)
+                        .withToPort(65535)
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllUDPFromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("udp")
+                        .withFromPort(0)
+                        .withToPort(65535)
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+
+    @Test
+    public void testAllICMPIPv6FromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("icmpv6")
+                        .withFromPort(-1)
+                        .withToPort(-1)
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllICMPIPv6FromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("icmpv6")
+                        .withFromPort(-1)
+                        .withToPort(-1)
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllICMPIPv4FromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("icmp")
+                        .withFromPort(-1)
+                        .withToPort(-1)
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllICMPIPv4FromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withIpProtocol("icmp")
+                        .withFromPort(-1)
+                        .withToPort(-1)
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testUnallowedPortFromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(9100)
+                        .withToPort(9100)
+                        .withIpProtocol("tcp")
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testUnallowedPortFromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(9100)
+                        .withToPort(9100)
+                        .withIpProtocol("tcp")
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
+        assertThat(pred).accepts(securityGroup);
+    }
+
+    @Test
+    public void testAllowedPortFromEverywhereIPv4() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(443)
+                        .withToPort(443)
+                        .withIpProtocol("tcp")
+                        .withIpv4Ranges(new IpRange().withCidrIp("0.0.0.0/0")));
+
+        assertThat(pred).rejects(securityGroup);
+    }
+
+    @Test
+    public void testAllowedPortFromEverywhereIPv6() throws Exception {
+        securityGroup.withIpPermissions(
+                new IpPermission()
+                        .withFromPort(443)
+                        .withToPort(443)
+                        .withIpProtocol("tcp")
+                        .withIpv6Ranges(new Ipv6Range().withCidrIpv6("::/0")));
+
         assertThat(pred).rejects(securityGroup);
     }
 }
