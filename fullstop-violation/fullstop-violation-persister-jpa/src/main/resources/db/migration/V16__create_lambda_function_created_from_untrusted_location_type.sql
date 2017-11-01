dc8365e0f4de3ec5b999b69d92936a4ff2d499f6
Michele Randi <michele.randi@gmail.com>
2017-11-01 13:51:12 +0100

    add db migration

--- fullstop-violation/fullstop-violation-persister-jpa/src/main/resources/db/migration/V16__create_lambda_function_created_from_untrusted_location_type.sql
+++ fullstop-violation/fullstop-violation-persister-jpa/src/main/resources/db/migration/V16__create_lambda_function_created_from_untrusted_location_type.sql
@@ -11,7 +11,7 @@ INSERT INTO fullstop_data.violation_type (
 VALUES ('LAMBDA_FUNCTION_CREATED_FROM_UNTRUSTED_LOCATION',
         'The code that has been used in a Lambda function, is not stored in one of the trusted S3 buckets.',
         1,
-        'Untrusted AWS-Lambda',
+        'Untrusted AWS-Lambda function',
         'pull/512',
         now(),
         now(),
