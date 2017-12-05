c2682b710bad08beb126b88d2b0e823594e10df0
Andre Hartmann <andre@zalando.de>
2017-12-05 15:50:07 +0100

    fix possible NullPointerException
    LambdaProperties#getS3Buckets threw a NullPointerException, when no
    config string was set.
    This change replaces the manual comma-separated-value parsing with the
    built-in Spring Boot functionality.

--- fullstop-plugins/fullstop-lambda-plugin/src/main/java/org/zalando/stups/fullstop/plugin/lambda/config/LambdaPluginProperties.java
+++ fullstop-plugins/fullstop-lambda-plugin/src/main/java/org/zalando/stups/fullstop/plugin/lambda/config/LambdaPluginProperties.java
@@ -1,25 +1,22 @@
 package org.zalando.stups.fullstop.plugin.lambda.config;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static com.google.common.collect.Lists.newArrayList;
 
 
 @ConfigurationProperties(prefix = "fullstop.plugins.lambda")
 public class LambdaPluginProperties {
 
-    private String s3Buckets;
+    private List<String> s3Buckets = newArrayList();
 
     public List<String> getS3Buckets() {
-        return Stream.of(s3Buckets.split(","))
-                .collect(toList());
+        return s3Buckets;
     }
 
-    public void setS3Buckets(final String s3Buckets) {
+    public void setS3Buckets(List<String> s3Buckets) {
         this.s3Buckets = s3Buckets;
     }
 }
