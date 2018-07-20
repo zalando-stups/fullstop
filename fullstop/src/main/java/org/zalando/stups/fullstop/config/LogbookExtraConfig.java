fabbd5f326cb2be12e2f31da8bed15688c2b01a4
Michele Randi <michele.randi@gmail.com>
2018-07-20 10:30:00 +0200

    move logbook

--- fullstop/src/main/java/org/zalando/stups/fullstop/config/LogbookExtraConfig.java
+++ fullstop/src/main/java/org/zalando/stups/fullstop/config/LogbookExtraConfig.java
@@ -1,16 +1,20 @@
 package org.zalando.stups.fullstop.config;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.BodyFilter;
 import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.JsonHttpLogFormatter;
+import org.zalando.logbook.spring.LogbookAutoConfiguration;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 
+
 @Configuration
-public class LogConfig {
+@AutoConfigureBefore(LogbookAutoConfiguration.class)
+public class LogbookExtraConfig {
 
     @Bean
     public HttpLogFormatter httpFormatter(final ObjectMapper objectMapper) {
@@ -18,5 +22,9 @@ public class LogConfig {
         mapper = mapper.disable(INDENT_OUTPUT);
         return new JsonHttpLogFormatter(mapper);
     }
-    
+
+    @Bean
+    BodyFilter hideFullBody() {
+        return (contentType, message) -> "<<obfuscated>>";
+    }
 }
